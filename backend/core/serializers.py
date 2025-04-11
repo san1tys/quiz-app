c5bb489ef1e05395f90fd15db739f69fe1e20e0b
Muslik <ahmetaliev345@gmail.com>
2025-04-12 02:03:36 +0500

    [fix] : registration passwords validation

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -13,12 +13,20 @@ class UserDetailSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class RegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)  
 
     class Meta:
         model = get_user_model()
-        fields = ['username', 'password', 'role']
+        fields = ['username', 'password', 'password2', 'role']
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
 
     def create(self, validated_data):
-        return get_user_model().objects.create_user(**validated_data)
+        validated_data.pop('password2') 
+        user = get_user_model().objects.create_user(**validated_data)
+        return user
 
