46127bf1c17e36bdd7e47c952252116c0e748ddd
Muslik <ahmetaliev345@gmail.com>
2025-04-13 02:39:14 +0500

    [add] : cors headers for frontend requests

--- backend/quiz_backend/settings.py
+++ backend/quiz_backend/settings.py
@@ -44,13 +44,15 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',  
     'core',
     'quiz',
-    'drf_yasg'
+    'drf_yasg',
+    'corsheaders'
 
 ]
 
 AUTH_USER_MODEL = 'core.User'
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,7 +60,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+]
+
 
 ROOT_URLCONF = 'quiz_backend.urls'
 
