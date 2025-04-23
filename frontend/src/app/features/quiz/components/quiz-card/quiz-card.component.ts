5745be32990cb9cdee783b2700ce8cd9777c555e
datkha <156799164+datkha@users.noreply.github.com>
2025-04-23 15:39:31 +0500

    upd

--- frontend/src/app/features/quiz/components/quiz-card/quiz-card.component.ts
+++ frontend/src/app/features/quiz/components/quiz-card/quiz-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Quiz } from '../../../../models/quiz';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-quiz-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './quiz-card.component.html',
   styleUrl: './quiz-card.component.scss',
 })
